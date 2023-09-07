68ade15a9be63dc3107282c0d40adb7f47186601
Francis-o <osazuwaidubor01@gmail.com>
2023-09-07 13:47:28 -0700

    fixing whitespace error in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,9 +2,9 @@
 /**
  * main - This program prints a sentence using the printf function in c 
  * Return: 0
- */
+*/
 int main(void)
 {
-printf("with proper grammar, but the outcome is a piece of art,");
+printf("with proper grammar, but the outcome is a piece of art, ");
 return (0);
 }
