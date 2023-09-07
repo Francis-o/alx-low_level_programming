9c2a8fe8b20db316b8187e0ddc35c055159fe04d
Francis-o <osazuwaidubor01@gmail.com>
2023-09-07 13:54:45 -0700

    g

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,7 +2,7 @@
 /**
  * main - This program prints a sentence using the printf function in c 
  * Return: 0
-*/
+ */
 int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art, ");
