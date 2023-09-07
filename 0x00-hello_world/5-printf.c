e5b2496dbe1dac96cdd4435cbb70938e10952082
Francis-o <osazuwaidubor01@gmail.com>
2023-09-07 13:41:31 -0700

    adding task-5_printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - This program prints a sentence using the printf function in c 
+ * Return: 0
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
