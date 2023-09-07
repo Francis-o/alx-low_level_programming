69c0896ca4bd06d25de505aa7511306a422dc937
Francis-o <osazuwaidubor01@gmail.com>
2023-09-07 13:05:58 -0700

    adding task-4_puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ * This program prints a sentence using the c language puts
+*/
+
+int main(void){
+
+    puts("Programming is like building a multilingual puzzle\n");	
+    return 0;
+}
