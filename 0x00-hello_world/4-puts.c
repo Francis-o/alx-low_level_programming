746183eaab9ed0b09e8155bb8bf1b4dae151bb98
Francis-o <osazuwaidubor01@gmail.com>
2023-09-07 13:33:31 -0700

    adjusting white spaces

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 /**
-* main - This program prints a sentence using the c language puts
-* Return: 0
+ * main - This program prints a sentence using the c language puts
+ * Return: 0
 */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");	
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
