838d8e37f8019ef43606e3dd580d0f86d6a36a23
Francis-o <osazuwaidubor01@gmail.com>
2023-09-08 14:33:28 -0700

    task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints lower alphabets except e and q
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alph[26] = "abcdfghijklmnoprstuvwxyz";
+        int i = 0;
+
+        while (i < 26)
+        {
+                putchar(alph[i]);
+                i++;
+        }
+        putchar('\n');
+        return (0);
+}
