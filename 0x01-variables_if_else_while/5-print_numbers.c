0b26e40f3ded8194734f83f1a19f6757cdc6bd94
Francis-o <osazuwaidubor01@gmail.com>
2023-09-08 15:08:41 -0700

    updating task 4 and 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i = 0;
+
+	while (i < 10)
+	{
+		printf("%d", i);
+		i++
+	}
+	putchar('\n');
+	return (0);
+}
+
