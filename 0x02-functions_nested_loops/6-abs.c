fe26fd649fb560e49238f6da927035e503e28651
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 12:39:24 -0700

    task 7

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _abs - Task 6
+ * Description - Returns absolute value of number
+ * @int: number
+ * Returns: integer
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+}
---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Task 7
+ * Description _ prints the last digit of number
+ * @int: Takes in integer value
+ * Returns: last digit value
+ */
+
+int print_last_digit(int n)
+{
+	return (n%10);
+}
