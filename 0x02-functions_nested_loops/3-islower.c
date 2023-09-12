4a25083c653f0e811ca3aa31a7f7d97ee5017051
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 10:42:59 -0700

    fixing task 3 error

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _islower - returns 1 if uppercase and 0 if lower
+ * @c: alphabet
+ * Return: returns 1 if lower or otherwise 0
+ */
+
+int _islower(int c)
+{
+	if (c > 'Z')
+	{
+		result = 1;
+	}
+	else
+	{
+		result = 0;
+	}
+	return (result);
+}
