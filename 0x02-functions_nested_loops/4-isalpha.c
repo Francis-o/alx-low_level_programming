9a4ed7d38f61962ad16422e18fe348c4325ddbee
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 11:48:59 -0700

    task 5

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _isalpha - Task 4
+ * Description - takes char and return 1 if alphabet.
+ * @c: takes in char value.
+ * Return: 1 if c is a letter otherwise 0.
+ */
+
+int _isalpha(int c)
+{
+	if (65 <= c <= 90 || 97 <= c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
