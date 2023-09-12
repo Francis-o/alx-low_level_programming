2f48609089c361e9c0ceb9fde1f6a4fc32825084
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 09:49:03 -0700

    betty doc error fix task 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,15 @@
 #include "main.h"
 
-/*
+/**
  * print_alphabet - prints all lower case alphabets
+ *
  */
 
 void print_alphabet(void)
 {
 	int i;
 
-	for (i = 'a'; i <= 'z'; i ++)
+	for (i = 'a'; i <= 'z'; i++)
 	{
 		_putchar(i);
 
