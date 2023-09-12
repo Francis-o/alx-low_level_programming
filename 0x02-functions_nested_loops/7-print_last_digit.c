27ddcd0931d8d20b48fa85eac9c9e965b3ce5f87
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 14:05:30 -0700

    adding other tasks

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,19 +3,19 @@
 /**
  * print_last_digit - Task 7
  * Description _ prints the last digit of number
- * @int: Takes in integer value
- * Returns: last digit value
+ * @n: Takes in integer value
+ * Return: last digit value
  */
 
 int print_last_digit(int n)
 {
 	int a;
 
-	a = n%10;
+	a = n % 10;
 	if (a < 0)
 	{
 		a = -a;
-	} 
+	}
 	_putchar(a + '0');
 	return (a);
 }
