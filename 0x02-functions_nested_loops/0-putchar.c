75ab96145b69720b6ba2339bee52e8ee74fc99e9
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 08:45:06 -0700

    task one correction

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,4 @@
-#include main.h
+#include "main.h"
 
 /*
  * main - The program prints _putchar with a new line
@@ -7,6 +7,14 @@
 
 int main(void)
 {
-	prinf("_putchar\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 	return (0);
 }
