c02da42c90f0623746532d87e44102d69569b563
Francis-o <osazuwaidubor01@gmail.com>
2023-09-12 16:10:56 -0700

    bl fix

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -7,16 +7,10 @@
 
 int main(void)
 {
-	unsigned long int i;
-	unsigned long int bef = 1;
-	unsigned long int aft = 2;
-	unsigned long int l = 1000000000;
-	unsigned long int bef1;
-	unsigned long int bef2;
-	unsigned long int aft1;
-	unsigned long int aft2;
-	printf("%lu", bef);
+	unsigned long int i, bef = 1, aft = 2, l = 1000000000;
+	unsigned long int bef1, bef2, aft1, aft2;
 
+	printf("%lu", bef);
 	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", aft);
