c6f6483b9f40b137738b6e4848a1258382ea480d
Francis-o <osazuwaidubor01@gmail.com>
2023-09-14 08:34:14 -0700

    task 5

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * more_numbers - prints 0 - 14 ten times
+ */
+
+void more_numbers(void)
+{
+	int count;
+	int num;
+	while (count < 9)
+	{
+		count++
+			for (num = 0; num <= 14; num++)
+			{
+				_putchar(num);
+			}
+		_putchar('\n');
+	}
+}
