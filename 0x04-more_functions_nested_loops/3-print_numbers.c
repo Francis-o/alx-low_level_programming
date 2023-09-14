8c25c55a375ec4b67b6c7222d512a909cff99beb
Francis-o <osazuwaidubor01@gmail.com>
2023-09-14 07:33:21 -0700

    task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h" 
+
+/**
+ * print_numbers - task 3 
+ * Description - prints numbers   
+ */ 
+
+void print_numbers() 
+{
+	for (int c = 0; C <= 9; c++)
+	{
+		_putchar(c);
+	}
+	_putchar('\n');
+}
