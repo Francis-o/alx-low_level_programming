bd887eb0edc321f2cec74880ce851ba44e367578
Francis-o <osazuwaidubor01@gmail.com>
2023-09-14 14:32:49 -0700

    task 0

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -6,6 +6,5 @@
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
 int convert_day(int month, int day);
-void print_remaining_days(int moonth, int day, int year);
 
 #endif /* MAIN_H */
