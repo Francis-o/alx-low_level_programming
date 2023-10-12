9c60a728d8492fb096ce1186c91887d320c6e114
francis-o <osazuwaidubor01@gmail.com>
2023-10-12 10:37:01 -0700

    error fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -16,7 +16,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	unsigned int index;
 
 	va_start(strings, n);
-	for (i = 0; index < n; i++)
+	for (i = 0; i < n; i++)
 	{
 		str = va_arg(strings, char *);
 		if (str == NULL)
