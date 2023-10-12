178cac748a44638d0042b5333cbe779117189fbb
francis-o <osazuwaidubor01@gmail.com>
2023-10-12 11:05:21 -0700

    update

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 {
 	va_list strings;
 	char *str;
-	unsigned int index;
+	unsigned int i;
 
 	va_start(strings, n);
 	for (i = 0; i < n; i++)
