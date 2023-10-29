206b3fbcf9a6e20dc6ae19b5fa5432570127a6d1
francis-o <osazuwaidubor01@gmail.com>
2023-10-28 21:07:49 -0700

    task 5 correction

--- 0x14-bit_manipulation/5-flip_bit.c
+++ 0x14-bit_manipulation/5-flip_bit.c
@@ -2,7 +2,6 @@
 
 /**
  * flip_bits - counts the number of bits to change
- * to get from one number to another
  * @n: first number
  * @m: second number
  * Return: number of bits to change
@@ -20,5 +19,6 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 		if (current & 1)
 			countbit++;
 	}
+
 	return (countbit);
 }
